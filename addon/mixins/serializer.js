85ce769a34178e570e17b1c2666d85ed9a4202eb
Bert De Block <bert.deblock@bagaar.be>
2019-02-20 11:40:48 +0100

    feat: Add serializer mixin

---
+++ addon/mixins/serializer.js
@@ -0,0 +1,28 @@
+import Mixin from '@ember/object/mixin';
+import { classify, underscore } from '@ember/string';
+
+export default Mixin.create({
+  /**
+   * Hooks
+   */
+
+  // Bagaaravel uses snake_case for attributes instead of kebab-case.
+  // "attributeName" -> "attribute_name"
+  keyForAttribute(key) {
+    return underscore(key);
+  },
+
+  // Bagaaravel uses the singular classified form of a model's name for "type".
+  // "model-name" -> "ModelName"
+  payloadKeyFromModelName(modelName) {
+    return classify(modelName);
+  },
+
+  // Bagaaravel only allows hasMany relationships to be saved via the record when it's new.
+  shouldSerializeHasMany(snapshot) {
+    let isRecordNew = snapshot.record.isNew;
+    let shouldSerializeHasMany = this._super(...arguments);
+
+    return isRecordNew && shouldSerializeHasMany;
+  },
+});
---
+++ tests/unit/mixins/serializer-test.js
@@ -0,0 +1,65 @@
+import SerializerMixin from '@bagaar/ember-data-bagaaravel/mixins/serializer';
+import JSONAPISerializer from 'ember-data/serializers/json-api';
+import { setupTest } from 'ember-qunit';
+import { module, test } from 'qunit';
+import createExistingRecord from '../../helpers/create-existing-record';
+
+module('Unit | Mixin | serializer', function (hooks) {
+  setupTest(hooks);
+
+  test('it underscores attributes', function (assert) {
+    let UserSerializer = JSONAPISerializer.extend(SerializerMixin);
+
+    this.owner.register('serializer:user', UserSerializer);
+
+    let store = this.owner.lookup('service:store');
+    let newUser = store.createRecord('user', { firstName: 'First Name' });
+    let serialized = newUser.serialize();
+
+    assert.equal(serialized.data.attributes.first_name, 'First Name');
+  });
+
+  test('it classifies the model name', function (assert) {
+    let UserSerializer = JSONAPISerializer.extend(SerializerMixin);
+
+    this.owner.register('serializer:user', UserSerializer);
+
+    let store = this.owner.lookup('service:store');
+    let newUser = store.createRecord('user');
+    let serialized = newUser.serialize();
+
+    assert.equal(serialized.data.type, 'User');
+  });
+
+  test('it serializes hasMany relationships for new records', function (assert) {
+    let UserSerializer = JSONAPISerializer.extend(SerializerMixin);
+
+    this.owner.register('serializer:user', UserSerializer);
+
+    let store = this.owner.lookup('service:store');
+    let newUser = store.createRecord('user');
+    let newProject = store.createRecord('project');
+
+    newUser.projects.addObject(newProject);
+
+    let serialized = newUser.serialize();
+
+    assert.ok(serialized.data.relationships.projects);
+  });
+
+  test('it does not serialize hasMany relationships for existing records', function (assert) {
+    let UserSerializer = JSONAPISerializer.extend(SerializerMixin);
+
+    this.owner.register('serializer:user', UserSerializer);
+
+    let store = this.owner.lookup('service:store');
+    let existingUser = createExistingRecord(store, 'user');
+    let newProject = store.createRecord('project');
+
+    existingUser.projects.addObject(newProject);
+
+    let serialized = existingUser.serialize();
+
+    assert.notOk(serialized.data.relationships);
+  });
+});
