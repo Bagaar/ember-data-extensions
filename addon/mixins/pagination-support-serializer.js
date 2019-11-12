1704e1038dfe26451b5e694d194e1660cc61e0b1
Bert De Block <bert.deblock@bagaar.be>
2019-11-12 11:57:52 +0100

    chore: deprecate the pagination-support-serializer mixin

--- addon/mixins/pagination-support-serializer.js
+++ addon/mixins/pagination-support-serializer.js
@@ -1,12 +1,27 @@
 /* eslint-disable ember/no-new-mixins */
 
 import addPaginationMeta from '@bagaaravel/ember-data-extensions/utils/add-pagination-meta'
+import { deprecate } from '@ember/debug'
 import Mixin from '@ember/object/mixin'
 
 export default Mixin.create({
   /**
    * Hooks
    */
+
+  init () {
+    this._super(...arguments)
+
+    deprecate(
+      'Use of the `pagination-support-serializer` mixin has been deprecated. Use the `add-pagination-meta` utility instead.',
+      false,
+      {
+        id:
+          '@bagaaravel/ember-data-extensions.pagination-support-serializer-mixin',
+        until: '1.0.0'
+      }
+    )
+  },
 
   normalizeQueryResponse () {
     let normalized = this._super(...arguments)
