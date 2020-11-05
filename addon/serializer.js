d159e099609948f5e964db34a723a8f0d5ddffc7
Bert De Block <bert.deblock@bagaar.be>
2020-11-05 11:54:25 +0100

    feat: add serialize util and extend shouldSerializeHasMany util

--- addon/serializer.js
+++ addon/serializer.js
@@ -1,3 +1,7 @@
+import {
+  getRelationshipDescriptor,
+  getRelationshipName
+} from '@bagaaravel/ember-data-extensions/-private/utils'
 import { classify, underscore } from '@ember/string'
 
 export function keyForAttribute (key) {
@@ -12,9 +16,48 @@ export function payloadKeyFromModelName (modelName) {
   return classify(modelName)
 }
 
-export function shouldSerializeHasMany (superCheck, snapshot) {
+export function serialize (serialized, snapshot) {
+  const relationshipName = getRelationshipName(snapshot.adapterOptions)
+  const isSavingRelationship = Boolean(relationshipName)
+
+  if (!isSavingRelationship) {
+    return serialized
+  }
+
+  const serializedRelationships = serialized.data.relationships
+
+  if (serializedRelationships && serializedRelationships[relationshipName]) {
+    return serializedRelationships[relationshipName]
+  }
+
+  const relationshipDescriptor = getRelationshipDescriptor(
+    snapshot.record,
+    relationshipName
+  )
+
+  if (relationshipDescriptor.kind === 'hasMany') {
+    return {
+      data: []
+    }
+  }
+
+  return {
+    data: null
+  }
+}
+
+export function shouldSerializeHasMany (
+  superCheck,
+  snapshot,
+  key,
+  relationshipDescriptor
+) {
   const isRecordNew = snapshot.record.isNew
   const isRecordSaving = snapshot.record.isSaving
+  const relationshipName = getRelationshipName(snapshot.adapterOptions)
+  const shouldSaveRelationship = relationshipName === relationshipDescriptor.key
 
-  return superCheck && (isRecordNew || !isRecordSaving)
+  return (
+    (superCheck && (isRecordNew || !isRecordSaving)) || shouldSaveRelationship
+  )
 }
--- tests/unit/serializer-test.js
+++ tests/unit/serializer-test.js
@@ -1,7 +1,9 @@
+import { saveRelationship } from '@bagaaravel/ember-data-extensions/model'
 import {
   keyForAttribute,
   keyForRelationship,
   payloadKeyFromModelName,
+  serialize,
   shouldSerializeHasMany
 } from '@bagaaravel/ember-data-extensions/serializer'
 import JSONAPIAdapter from '@ember-data/adapter/json-api'
@@ -10,37 +12,50 @@ import { setupTest } from 'ember-qunit'
 import { module, test } from 'qunit'
 import createExistingRecord from '../helpers/create-existing-record'
 
+class UserSerializer extends JSONAPISerializer {
+  keyForAttribute () {
+    return keyForAttribute(...arguments)
+  }
+
+  keyForRelationship () {
+    return keyForRelationship(...arguments)
+  }
+
+  payloadKeyFromModelName () {
+    return payloadKeyFromModelName(...arguments)
+  }
+
+  serialize () {
+    const serialized = super.serialize(...arguments)
+
+    return serialize(serialized, ...arguments)
+  }
+
+  shouldSerializeHasMany () {
+    const superCheck = super.shouldSerializeHasMany(...arguments)
+
+    return shouldSerializeHasMany(superCheck, ...arguments)
+  }
+}
+
 module('Unit | Serializer', function (hooks) {
   setupTest(hooks)
 
-  test('it underscores attributes', function (assert) {
-    class UserSerializer extends JSONAPISerializer {
-      keyForAttribute () {
-        return keyForAttribute(...arguments)
-      }
-    }
-
+  hooks.beforeEach(function () {
+    this.store = this.owner.lookup('service:store')
     this.owner.register('serializer:user', UserSerializer)
+  })
 
-    const store = this.owner.lookup('service:store')
-    const newUser = store.createRecord('user', { firstName: 'First Name' })
+  test('it underscores attributes', function (assert) {
+    const newUser = this.store.createRecord('user', { firstName: 'First Name' })
     const serialized = newUser.serialize()
 
     assert.equal(serialized.data.attributes.first_name, 'First Name')
   })
 
   test('it leaves relationships untouched', function (assert) {
-    class UserSerializer extends JSONAPISerializer {
-      keyForRelationship () {
-        return keyForRelationship(...arguments)
-      }
-    }
-
-    this.owner.register('serializer:user', UserSerializer)
-
-    const store = this.owner.lookup('service:store')
-    const newUser = store.createRecord('user')
-    const newProject = store.createRecord('project')
+    const newUser = this.store.createRecord('user')
+    const newProject = this.store.createRecord('project')
 
     newUser.favoriteProjects.addObject(newProject)
 
@@ -50,35 +65,15 @@ module('Unit | Serializer', function (hooks) {
   })
 
   test('it classifies the model name', function (assert) {
-    class UserSerializer extends JSONAPISerializer {
-      payloadKeyFromModelName () {
-        return payloadKeyFromModelName(...arguments)
-      }
-    }
-
-    this.owner.register('serializer:user', UserSerializer)
-
-    const store = this.owner.lookup('service:store')
-    const newUser = store.createRecord('user')
+    const newUser = this.store.createRecord('user')
     const serialized = newUser.serialize()
 
     assert.equal(serialized.data.type, 'User')
   })
 
   test('it serializes hasMany relationships for new records', function (assert) {
-    class UserSerializer extends JSONAPISerializer {
-      shouldSerializeHasMany () {
-        const superCheck = super.shouldSerializeHasMany(...arguments)
-
-        return shouldSerializeHasMany(superCheck, ...arguments)
-      }
-    }
-
-    this.owner.register('serializer:user', UserSerializer)
-
-    const store = this.owner.lookup('service:store')
-    const newUser = store.createRecord('user')
-    const newProject = store.createRecord('project')
+    const newUser = this.store.createRecord('user')
+    const newProject = this.store.createRecord('project')
 
     newUser.projects.addObject(newProject)
 
@@ -88,19 +83,8 @@ module('Unit | Serializer', function (hooks) {
   })
 
   test('it serializes hasMany relationships for existing records', function (assert) {
-    class UserSerializer extends JSONAPISerializer {
-      shouldSerializeHasMany () {
-        const superCheck = super.shouldSerializeHasMany(...arguments)
-
-        return shouldSerializeHasMany(superCheck, ...arguments)
-      }
-    }
-
-    this.owner.register('serializer:user', UserSerializer)
-
-    const store = this.owner.lookup('service:store')
-    const existingUser = createExistingRecord(store, 'user')
-    const newProject = store.createRecord('project')
+    const existingUser = createExistingRecord(this.store, 'user')
+    const newProject = this.store.createRecord('project')
 
     existingUser.projects.addObject(newProject)
 
@@ -118,23 +102,109 @@ module('Unit | Serializer', function (hooks) {
       }
     }
 
-    class UserSerializer extends JSONAPISerializer {
-      shouldSerializeHasMany () {
-        const superCheck = super.shouldSerializeHasMany(...arguments)
-
-        return shouldSerializeHasMany(superCheck, ...arguments)
-      }
-    }
-
     this.owner.register('adapter:user', UserAdapter)
-    this.owner.register('serializer:user', UserSerializer)
 
-    const store = this.owner.lookup('service:store')
-    const existingUser = createExistingRecord(store, 'user')
-    const newProject = store.createRecord('project')
+    const existingUser = createExistingRecord(this.store, 'user')
+    const newProject = this.store.createRecord('project')
 
     existingUser.projects.addObject(newProject)
 
     await existingUser.save()
+  })
+
+  test('saving a record', async function (assert) {
+    let serialized
+
+    class UserAdapter extends JSONAPIAdapter {
+      ajax (url, type, options) {
+        serialized = options.data
+      }
+    }
+
+    this.owner.register('adapter:user', UserAdapter)
+
+    const existingUser = createExistingRecord(this.store, 'user', {
+      firstName: 'First Name'
+    })
+
+    await existingUser.save()
+
+    assert.deepEqual(serialized, {
+      data: {
+        attributes: {
+          first_name: 'First Name'
+        },
+        id: existingUser.id,
+        type: 'User'
+      }
+    })
+  })
+
+  test('saving a belongsTo relationship', async function (assert) {
+    let serialized
+
+    class UserAdapter extends JSONAPIAdapter {
+      ajax (url, type, options) {
+        serialized = options.data
+      }
+    }
+
+    this.owner.register('adapter:user', UserAdapter)
+
+    const existingUser = createExistingRecord(this.store, 'user')
+
+    await saveRelationship(existingUser, 'company')
+
+    assert.deepEqual(serialized, {
+      data: null
+    })
+
+    const existingCompany = createExistingRecord(this.store, 'company')
+
+    existingUser.set('company', existingCompany)
+
+    await saveRelationship(existingUser, 'company')
+
+    assert.deepEqual(serialized, {
+      data: {
+        id: existingCompany.id,
+        type: 'Company'
+      }
+    })
+  })
+
+  test('saving an hasMany relationship', async function (assert) {
+    let serialized
+
+    class UserAdapter extends JSONAPIAdapter {
+      ajax (url, type, options) {
+        serialized = options.data
+      }
+    }
+
+    this.owner.register('adapter:user', UserAdapter)
+
+    const existingUser = createExistingRecord(this.store, 'user')
+
+    await saveRelationship(existingUser, 'projects')
+
+    assert.deepEqual(serialized, {
+      data: []
+    })
+
+    const existingProject = createExistingRecord(this.store, 'project')
+
+    existingUser.projects.addObject(existingProject)
+
+    await saveRelationship(existingUser, 'projects')
+
+    assert.deepEqual(serialized, {
+      data: [
+        {
+          id: existingProject.id,
+          type: 'Project'
+        }
+      ]
+    })
   })
 })
